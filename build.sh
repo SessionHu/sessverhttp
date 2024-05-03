6c38d2313e0a48ff6d0c51e31065802ecf3ee5b9
SessionHu <102411014+SessionHu@users.noreply.github.com>
2024-05-03 15:15:08 +0800

    chore: build

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
+#!/bin/sh
+
 # https://github.com/SessionHu/psb4j
 
-psb4j_args="--extra-packin ./README.md,./LICENSE --jar ./build/sessverhttp.jar"
+psb4j_args="--extra-packin ./README.md,./LICENSE --jar ./build/sessverhttp.jar --clear"
 
 if [ -s ~/.sessx/lib/psb4j.jar ]; then
     java -jar ~/.sessx/lib/psb4j.jar ${psb4j_args}
 else
-    if [ -s ./psb4j.jar] ; then
+    if [ -s ./psb4j.jar ] ; then
         java -jar ./psb4j.jar ${psb4j_args}
     else
-        wget https://github.com/SessionHu/psb4j/releases/latest/download/psb4j.jar -O psb4j.jar
-        java -jar ./psb4j.jar ${psb4j_args}
+        wget https://github.com/SessionHu/psb4j/releases/latest/download/psb4j.jar -O ~/.sessx/lib/psb4j.jar
+        java -jar ~/.sessx/lib/psb4j.jar ${psb4j_args}
     fi
 fi
