43000a59a757a1e445303ab05651e8e3a40bcd76
SessionHu <102411014+SessionHu@users.noreply.github.com>
2024-06-28 12:05:29 +0800

    chore: better build.sh

--- build.sh
+++ build.sh
@@ -3,14 +3,20 @@
 # https://github.com/SessionHu/psb4j
 
 psb4j_args="--extra-packin ./README.md,./LICENSE --jar ./build/sessverhttp.jar --clear"
+psb4j_jar_path="${HOME}/.sessx/lib/psb4j.jar"
 
-if [ -s ~/.sessx/lib/psb4j.jar ]; then
-    java -jar ~/.sessx/lib/psb4j.jar ${psb4j_args}
+run_psb4j() {
+    java -jar $1 ${psb4j_args} || echo "Error: Failed to run psb4j with JAR $1"
+}
+
+if [ -s ${psb4j_jar_path} ]; then
+    run_psb4j ${psb4j_jar_path}
 else
-    if [ -s ./psb4j.jar ] ; then
-        java -jar ./psb4j.jar ${psb4j_args}
+    if [ -s ./psb4j.jar ]; then
+        run_psb4j ./psb4j.jar
     else
-        wget https://github.com/SessionHu/psb4j/releases/latest/download/psb4j.jar -O ~/.sessx/lib/psb4j.jar
-        java -jar ~/.sessx/lib/psb4j.jar ${psb4j_args}
+        mkdir -p ${HOME}/.sessx/lib
+        wget https://github.com/SessionHu/psb4j/releases/latest/download/psb4j.jar -O ${psb4j_jar_path} && \
+        run_psb4j ${psb4j_jar_path} || echo "Error: Failed to download psb4j.jar"
     fi
 fi
